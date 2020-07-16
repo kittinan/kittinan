cdb94ac5f8919b8cfccb72ef6bffb5e235a6f89c
Kittinan Srithaworn <tunxedge@gmail.com>
2020-07-16 08:41:15 +0700

    spotify refresh token

--- api/spotify-playing.py
+++ api/spotify-playing.py
@@ -1,19 +1,64 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
+from base64 import b64encode
 
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy import SpotifyOAuth
-
-import spotipy
+import requests
 import json
+import os
+
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
+SPOTIFY_REFRESH_TOKN = os.getenv("SPOTIFY_REFRESH_TOKN")
+
+SPOTIFY_URL_REFRESH_TOKEN = "https://accounts.spotify.com/api/token"
+SPOTIFY_URL_NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing"
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    username = "21jsj34glwsu3dboqjpqzm2sa"
-    scope = "user-read-currently-playing"
 
-    auth_manager = SpotifyOAuth(username=username, scope=scope)
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    return Response(json.dumps(sp.current_user_playing_track()), mimetype="text/json")
+def get_authorization():
+
+    return b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET_ID}".encode()).decode("ascii")
+
+
+def refresh_token():
+
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": SPOTIFY_REFRESH_TOKN,
+    }
+
+    headers = {"Authorization": "Basic {}".format(get_authorization())}
+
+    response = requests.post(SPOTIFY_URL_REFRESH_TOKEN, data=data, headers=headers)
+    repsonse_json = response.json()
+
+    return repsonse_json["access_token"]
+
+
+def get_now_playing():
+
+    token = refresh_token()
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    response = requests.get(SPOTIFY_URL_NOW_PLAYING, headers=headers)
+
+    if response.status_code == 204:
+        return {}
+
+    repsonse_json = response.json()
+    return repsonse_json
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+
+    r = get_now_playing()
+
+    return jsonify(r)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- api/tun.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Response
-app = Flask(__name__)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
