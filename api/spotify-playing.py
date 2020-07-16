0cfb090e944f994212dcd27158e52dcd3379e747
Kittinan Srithaworn <tunxedge@gmail.com>
2020-07-16 10:34:35 +0700

    svg playing output

--- api/spotify-playing.py
+++ api/spotify-playing.py
@@ -51,13 +51,80 @@ def get_now_playing():
     return repsonse_json
 
 
+def get_svg_template():
+
+    svg = """
+        <svg width="200" height="275" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <foreignObject width="200" height="275">
+                <div xmlns="http://www.w3.org/1999/xhtml">
+                    <style>
+                        .playing {{ font: bold 18px serif; color: red; text-align: center; }}
+                        .artist {{ font: bold 18px serif; text-align: center; }}
+                    </style>
+                    {}
+                </div>
+            </foreignObject>
+        </svg>
+    """
+    return svg
+
+
+def load_image_b64(url):
+
+    resposne = requests.get(url)
+    return b64encode(resposne.content).decode("ascii")
+
+
+def make_svg(data):
+
+    template = get_svg_template()
+
+    if data == {}:
+        content = """
+            <div class="playing">Nothing playing on Splotify</div>
+        """
+        return template.format(content)
+
+    content = """
+        <div class="playing">Now playing on Splotify</div>
+        <br />
+        <div class="artist">{}</div>
+        <br />
+        <a href="{}" target="_BLANK">
+            <img src="data:image/png;base64, {}" height="200"/>
+        </a>
+        """
+
+    item = data["item"]
+
+    """
+    print(json.dumps(item))
+    print(item["artists"][0]["name"])
+    print(item["external_urls"]["spotify"])
+    print(item["album"]["images"][0]["url"])
+    """
+
+    img = load_image_b64(item["album"]["images"][0]["url"])
+    content_rendered = content.format(
+        item["artists"][0]["name"], item["external_urls"]["spotify"], img,
+    )
+
+    return template.format(content_rendered)
+
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
 
-    r = get_now_playing()
+    # TODO: caching
 
-    return jsonify(r)
+    data = get_now_playing()
+    svg = make_svg(data)
+
+    resp = Response(svg, mimetype="image/svg+xml")
+    resp.headers["Cache-Control"] = "s-maxage=1"
+
+    return resp
 
 
 if __name__ == "__main__":
